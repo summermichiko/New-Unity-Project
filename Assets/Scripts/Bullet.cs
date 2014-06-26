695d79aaaaa405268d9c169705d59e83fd7ebe47
Summer <summermichiko@gmail.com>
2014-06-25 22:32:49 -0400

    initial commit - true Unity files

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float deathTimer;
+	
+	// Use this for initialization
+	void Start () {
+		deathTimer = 5f;
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (10f, 0, 0) * Time.deltaTime);
+		if (deathTimer > 0) {
+			deathTimer = deathTimer - Time.deltaTime;
+		}
+		if (deathTimer < 0) {
+			Destroy(gameObject);		
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float speed = 40;
+	public CharacterController cController;
+	public GameObject bullet;
+	public int enemiesHit = 0;C
+
+	// Use this for initialization
+	void Start () {
+		//prints "CUBE!" to the console
+		Debug.Log ("CUBE!");
+		cController = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//can't be dEbUg.lOg();
+		Debug.Log ("CUUUUBE!");
+
+		if (Input.GetButton ("Right")) {
+			cController.Move (new Vector3(speed, 0, 0)  * Time.deltaTime);
+		}
+
+		if (Input.GetButton ("Left")) {
+			cController.Move (new Vector3(-speed, 0, 0)  * Time.deltaTime);
+		}
+
+		cController.Move (new Vector3(0, -9.81f, 0)  * Time.deltaTime);
+
+		if (Input.GetButton ("Jump")) {
+			cController.Move (new Vector3(0, speed, 0)  * Time.deltaTime);
+		}
+
+		if (Input.GetButton ("Shoot")) {
+			Instantiate(bullet, transform.position, Quaternion.identity);
+		}
+	}
+
+}
